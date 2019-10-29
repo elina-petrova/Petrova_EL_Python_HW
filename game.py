a3321cf29ffe7f36aa1367e0642a6187d106fea1
Petrova <e_petrova@ats-hr420-dm16.fc.ca>
2019-10-29 13:33:35 -0400

    newpython

--- game.py
+++ game.py
@@ -14,16 +14,43 @@ computer = choices[randint(0, 2)]
 
 player = False 
 while player == False:
+	print("*******************************\n\n")
+	print("Choose your weapon!\n\n")
+	print("*****************\n\n")
 
-	player = input("choose rock, paper ot scissors\n")
+	player = input("choose rock, paper ot scissors: ")
+	print("*****************")
 
 	print("computer chose ", computer, "\n")
 	print("player chose ", player, "\n")
 
-	if player =="quit":
+	if player.lower() =="quit":
 		exit()
-	elif computer == player:
+	elif computer == player.lower():
 		print("tie! no one wins, play again")
+
+	elif player.lower() == "rock":
+		if computer == "paper":
+			print("Y O U    L O S E ! ", computer, "covers", player, "\n")
+		else:
+			print("Y O U  W I N ! ", player, "smashes", computer, "\n")
+
+	elif player.lower() == "paper":
+		if computer == "scissors":
+			print("Y O U    L O S E ! ", computer, "cuts", player, "\n")
+		else:
+			print("Y O U  W I N ! ", player, "smashes", computer, "\n")
+
+	elif player.lower() == "scissors":
+		if computer == "rock":
+			print("Y O U    L O S E ! ", computer, "smashes", player, "\n")
+		else:
+			print("Y O U  W I N ! ", player, "cuts", computer, "\n")
+
+	else:
+		print("That's not a valid choice, try again")
+
+
 
 	#need ro check all of our comditions after checking for a tie
 	player = False;
